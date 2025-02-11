185cf6f6b3e7d9a499f711b8ce73dd65cd7bb9d7
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2025-02-11 16:04:15 +0530

    basic pattern 9

---
+++ BasicPattern9.java
@@ -0,0 +1,12 @@
+public class BasicPattern9 {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            int x=i;
+            for(int j=1;j<=5;j++){
+                System.out.print(x+" ");
+                x++;
+            }
+            System.out.println();
+        }
+    }
+}
